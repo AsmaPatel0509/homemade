1148202c650a1186c4390abb2dfd0ad55418ec47
hellym20 <38601719+hellym20@users.noreply.github.com>
2018-04-24 00:50:55 +0530

    Add files via upload

--- cart.php
+++ cart.php
@@ -11,6 +11,12 @@
 
 <?php 
 
+	$dbuser="root";
+	$dbpass="";
+	$host="localhost";
+	$db="home_made";
+	$conn =mysql_connect($host,$dbuser,$dbpass);
+	mysql_select_db($db,$conn);
 	 if(!isset($_SESSION['login_session']))
 	 {			
 			include('header.html'); 		
@@ -20,30 +26,37 @@
 			include('headerlogout.html'); 
 	 }	
 	 $u_id=$_SESSION['login_session'];
-	 $list=$_POST['list'];
+		echo "<script> alert('$u_id'); </script>";
 	 if($_POST['s'])
 	 {
-	 	foreach ($list as $x) {
+		
+		$lists=$_POST['list'];
+		$id=$_POST['i'];
+		$m_id=$_POST['mid'];
+		//echo $lists;
+	 	foreach ($lists as $x) {
 	 		# code...
-	 		$query1="UPDATE pending SET status='1' WHERE u_id='$u_id'";
-	 		$result1=mysql_query($conn,$query1);
-	 		//$query2="SELECT * from pending";
-	 		//$result=mysql_query($query,$conn) or die($query."<br/><br/>".mysql_error());
-	 		//$u_id=$
-
-	 	}
+			$is_checked = ($x == "0" ? '1' : '0');
+			if($is_checked=='1')
+			{
+				echo $x;
+				$query1="UPDATE pending SET status='1' where order_id='$id'";
+				$result1=mysql_query($query1,$conn);
+				
+				//$query2="SELECT * from pending where status='1'";
+				//$result2=mysql_query($conn,$query2) or die($query."<br/><br/>".mysql_error());
+				//$query3="insert into final_order(u_id,m_id,p_name,qty,price,amount) values('$u_id','$m_id','$p_name','$qty','$price','$amt')";
+				//$result3=mysql_query($conn,$query3) or die($query."<br/><br/>".mysql_error());
+			}
+		}
+	 	
 
 	 }
 ?>
 
 	<h1>My Orders</h1><br>
 	<?php 
-		$dbuser="root";
-		$dbpass="";
-		$host="localhost";
-		$db="home_made";
-		$conn =mysql_connect($host,$dbuser,$dbpass);
-		mysql_select_db($db,$conn);
+		
 
 		$user_check=$_SESSION['login_session'];
 		//echo $user_check;
@@ -51,30 +64,22 @@
 		$result=mysql_query($query,$conn) or die($query."<br/><br/>".mysql_error());
 		//echo $result;
 	?>
-	<table>
+	
+	<form method="post">
 		<?php
 			while ($row=mysql_fetch_array($result)) {
-		?>		
-
-			<tr>
-				<td><?php echo $row['p_name'] ?></td>
+		?>	
+			<input type="text" name="p_name" value="<?php echo $row['p_name']; ?>" />
+			<input type="text" name="qty" value="<?php echo $row['qty']; ?>" />
+			<input type="text" name="price" value="<?php echo $row['price']; ?>" />
+			<input type="text" name="amt" value="<?php echo $row['qty']*$row['price']; ?>" />
+			<input type="text" name="i" value="<?php echo $row['order_id']; ?>" hidden="hidden" />
+			<input type="text" name="mid" value="<?php echo $row['m_id']; ?>" hidden="hidden" />
+			<input type="checkbox" name="list[]" id="list" value="0" ></br>
 			
-				<td><?php echo $row['price'] ?></td>
-			
-				<td><?php echo $row['qty'] ?></td>
-			
-				<td><?php
-						$amt=$row['price']*$row['qty'];
-				 echo $amt ?></td>
-			
-				<td><input type="checkbox" name="list[]"></td>
-			</tr>
-<?php
+		<?php
 			}
-		
-?>
-
-	</table>
+		?>
 	<input type="submit" name="s" value="Confirm Order">
 </body>
 </html>	
